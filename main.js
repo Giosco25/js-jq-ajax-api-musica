deaae825dc124ec3b443f520798442a205173a30
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-05-20 18:00:22 +0200

    inizio select

--- main.js
+++ main.js
@@ -16,14 +16,14 @@ $(document).ready(function() {
            // Ciclare quindi i dischi e ottenuti
            for (var i = 0; i < musica.length; i++) {
 
-               var ciclo_musica = musica[i];
-               console.log(ciclo_musica);
+               var dischi_musica = musica[i];
+               console.log(dischi_musica);
                // per ognuno di essi disegnare in pagina una card utilizzando handlebars.
                var segnaposto = {
-                   'poster' : ciclo_musica.poster,
-                   'titolo' : ciclo_musica.title,
-                   'artista': ciclo_musica.author,
-                   'anno' :  ciclo_musica.year
+                   'poster' : dischi_musica.poster,
+                   'titolo' : dischi_musica.title,
+                   'artista': dischi_musica.author,
+                   'anno' :  dischi_musica.year
                }
                var html_generata = template_function(segnaposto);
                $('.cds-container').append(html_generata);
@@ -33,15 +33,3 @@ $(document).ready(function() {
 });
 
 // BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal").
-
-function myFunction() {
-  var x = document.createElement("SELECT");
-  x.setAttribute("id", "mySelect");
-  document.body.appendChild(x);
-
-  var z = document.createElement("option");
-  z.setAttribute("name", "volvocar");
-  var t = document.createTextNode("Volvo");
-  z.appendChild(t);
-  document.getElementById("mySelect").appendChild(z);
-}
