009791137671cf5006e69d78062f9617c23d1646
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-05-20 15:45:28 +0200

    prova per disegnare le card in pagina

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	//Code
     // Chiamata ajax per recupere i dati musicali
+    var html_template = $('#entry-template').html();
+    var template_function = Handlebars.compile(html_template);
     $.ajax({
        'url':'https://flynn.boolean.careers/exercises/api/array/music',
        'method': 'GET',
@@ -13,11 +15,11 @@ $(document).ready(function() {
            for (var i = 0; i < musica.length; i++) {
                var ciclo_musica = musica[i];
                console.log(ciclo_musica);
-           }
+               // per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+               $('.cds-container').append('<div class="cd"></div>').val(ciclo_musica);
        }
     })
 });
 
 
-// Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
 // BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal").
